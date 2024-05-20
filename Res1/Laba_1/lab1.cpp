dc8f94f5d8658cac6f49f6be35a36ef88eb33e66
mttUi <158287701+mttUi@users.noreply.github.com>
2024-05-20 19:35:51 +0200

    did laba_1

---
+++ Res1/Laba_1/lab1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//Целочисленные типы 
+//short - 2 байта, -2^15...2^15-1
+//int - 4 байта, -2^31...2^31-1
+//long - 8 байт, -2^31...2^31-1
+//long long - 8 байт, -2^63...2^63-1 
+
+//Вещественные типы
+//float - 4 байта, 7 значащих цифр
+//double - 8 байт, 15 значащих цифр
+//long double -  тоже что и double
+
+int main() {
+    double h, S;
+    long double a;
+    cout << "S= "; cin >> S;
+    cout << "h= "; cin >> h;
+    a = (2 * S) / h;
+    cout << "a= " << a << endl;
+    return 0;
+}
